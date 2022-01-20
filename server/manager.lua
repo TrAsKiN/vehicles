b494b9003567b954bbf244607de8fe81e8cd79b0
MARCHAL Simon <marchal.simon@gmail.com>
2022-01-20 19:31:26 +0100

    Synchronize only if an entity has been deleted Adding verbosity

--- server/manager.lua
+++ server/manager.lua
@@ -2,11 +2,14 @@ local vehicles = {}
 
 RegisterNetEvent('vehicle:data:init')
 AddEventHandler('vehicle:data:init', function()
-    TriggerClientEvent('vehicle:data:sync', source, vehicles)
+    local playerId = source
+    print("Retrieve vehicle data...", "Player: ".. playerId)
+    TriggerClientEvent('vehicle:data:sync', playerId, vehicles)
 end)
 
 RegisterNetEvent('vehicle:data:toSync')
 AddEventHandler('vehicle:data:toSync', function(vehicleId, name, data)
+    print("Receiving data to synchronize...", "From: ".. source)
     if not vehicles[vehicleId] then
         vehicles[vehicleId] = {}
     end
@@ -15,10 +18,15 @@ AddEventHandler('vehicle:data:toSync', function(vehicleId, name, data)
 end)
 
 AddEventHandler('playerEnteredScope', function (data)
-    TriggerClientEvent('vehicle:data:sync', data.player, vehicles)
+    print("Two players meeting, forcing synchronization...", data['player'] .." meet ".. data['for'])
+    TriggerClientEvent('vehicle:data:sync', data['for'], vehicles)
+    TriggerClientEvent('vehicle:data:sync', data['player'], vehicles)
 end)
 
 AddEventHandler('entityRemoved', function(entityId)
-    vehicles[entityId] = nil
-    TriggerClientEvent('vehicle:data:sync', -1, vehicles)
+    if vehicles[entityId] then
+        print("A synchronized entity no longer exists, removing...", "Entity: ".. entityId)
+        vehicles[entityId] = nil
+        TriggerClientEvent('vehicle:data:sync', -1, vehicles)
+    end
 end)
