860e9e623b52c37e9cbe1af4ac17f47c89d37e89
MARCHAL Simon <marchal.simon@gmail.com>
2022-01-22 14:26:35 +0100

    Add animation and sound for doors system

--- client/doors.lua
+++ client/doors.lua
@@ -1,8 +1,10 @@
 local DOORS_SYSTEM = GetConvarInt('doorsSystem', 0)
 local DOORS_INPUT = GetConvar('doorsInput', 'U')
+local ANIMATION_DICTIONARY = 'anim@mp_player_intmenu@key_fob@'
 
 if DOORS_SYSTEM then
     RegisterCommand('vehicle:doors:toggle', function()
+        local ahead = false
         local playerPed = PlayerPedId()
         local vehicle = nil
         if IsPedInAnyVehicle(playerPed) then
@@ -12,13 +14,24 @@ if DOORS_SYSTEM then
             end
         else
             vehicle = getVehicleAhead()
+            ahead = true
+            RequestAnimDict(ANIMATION_DICTIONARY)
+            repeat Wait(100) until HasAnimDictLoaded(ANIMATION_DICTIONARY)
         end
         if vehicle then
             if GetVehicleDoorLockStatus(vehicle) > 1 then
                 TriggerServerEvent('vehicle:data:toSync', VehToNet(vehicle), 'doors', 1)
+                if ahead then
+                    TaskPlayAnim(playerPed, ANIMATION_DICTIONARY, 'fob_click_fp', 8.0, 8.0, -1, 48, 1, false, false, false)
+                    PlaySoundFromEntity(-1, 'Launch', playerPed, 'DLC_H3_Tracker_App_Sounds')
+                end
             else
                 SetVehicleDoorsShut(vehicle, false)
                 TriggerServerEvent('vehicle:data:toSync', VehToNet(vehicle), 'doors', 2)
+                if ahead then
+                    TaskPlayAnim(playerPed, ANIMATION_DICTIONARY, 'fob_click_fp', 8.0, 8.0, -1, 48, 1, false, false, false)
+                    PlaySoundFromEntity(-1, 'Close', playerPed, 'DLC_H3_Tracker_App_Sounds')
+                end
             end
         end
     end, true)
