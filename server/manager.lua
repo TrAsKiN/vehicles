d8c8478cee8167f84b55912571b5071c319afed3
TrAsKiN <marchal.simon@gmail.com>
2022-05-08 19:00:53 +0200

    fix: initialize value for all vehicles

--- server/manager.lua
+++ server/manager.lua
@@ -1,9 +1,5 @@
 RegisterNetEvent('entityCreated', function (entity)
-    if GetEntityType(entity) == 2 and HasVehicleBeenOwnedByPlayer(entity) then
+    if GetEntityType(entity) == 2 then
         Entity(entity).state:set('vehiclesInit', true, true)
     end
-end)
-
-AddStateBagChangeHandler(nil, nil, function (bagName, key, value, reserved, replicated)
-    -- log.debug(string.format("%s: %s = %s (replicated: %s)", bagName, key, type(value) == 'table' and json.encode(value, {indent = true}) or tostring(value), tostring(replicated)))
 end)
