ae17bfb1eee2d4ab6214c9a568937b5f1a441e3d
TrAsKiN <marchal.simon@gmail.com>
2022-05-10 12:04:27 +0200

    chore: check if already set

--- server/manager.lua
+++ server/manager.lua
@@ -1,5 +1,5 @@
 RegisterNetEvent('entityCreated', function (entity)
-    if GetEntityType(entity) == 2 then
+    if GetEntityType(entity) == 2 and not Entity(entity).state.vehiclesInit then
         Entity(entity).state:set('vehiclesInit', true, true)
     end
 end)
