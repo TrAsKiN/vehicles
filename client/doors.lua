6c99f3a80cb112c498f1439a020d69ced0dab984
TrAsKiN <marchal.simon@gmail.com>
2022-05-22 12:04:43 +0200

    fix: correct deadlock in a loop

--- client/doors.lua
+++ client/doors.lua
@@ -1,34 +1,9 @@
 local DOORS_SYSTEM = GetConvarInt('doorsSystem', 0)
 local DOORS_INPUT = GetConvar('doorsInput', 'U')
-local ANIMATION_DICTIONARY = 'anim@mp_player_intmenu@key_fob@'
-local KEY_PROP = 'lr_prop_carkey_fob'
 
 local hasKeyCallback = function (vehicle) return true end
 
 if DOORS_SYSTEM then
-    local function playKeyAnimation(onPed)
-        while not HasAnimDictLoaded(ANIMATION_DICTIONARY) do
-            RequestAnimDict(ANIMATION_DICTIONARY)
-            Wait(0)
-        end
-        while not HasModelLoaded(KEY_PROP) do
-            RequestModel(KEY_PROP)
-            Wait(0)
-        end
-        local x, y, z = table.unpack(GetEntityCoords(onPed))
-        local key = CreateObject(KEY_PROP, x, y, z + 0.2, true, true, true)
-        SetEntityAsMissionEntity(key, true, true)
-        AttachEntityToEntity(key, onPed, GetPedBoneIndex(onPed, 57005), 0.14, 0.04, -0.0175, -110.0, 95.0, -10.0, true, true, false, true, 1, true)
-        TaskPlayAnim(onPed, ANIMATION_DICTIONARY, 'fob_click_fp', 8.0, 8.0, -1, 48, 1, false, false, false)
-        CreateThread(function()
-            Wait(1200)
-            SetModelAsNoLongerNeeded(KEY_PROP)
-            RemoveAnimDict(ANIMATION_DICTIONARY)
-            DetachEntity(key, false, false)
-            DeleteObject(key)
-        end)
-    end
-
     RegisterCommand('vehicle:doors:toggle', function()
         local key = nil
         local ahead = false
@@ -43,9 +18,7 @@ if DOORS_SYSTEM then
             vehicle = getVehicleAhead()
             ahead = true
         end
-        if vehicle and hasKeyCallback(vehicle) then
-            NetworkRequestControlOfEntity(vehicle)
-            repeat Wait(0) until NetworkHasControlOfEntity(vehicle)
+        if vehicle and hasKeyCallback(vehicle) and control(vehicle) then
             if GetVehicleDoorLockStatus(vehicle) > 1 then
                 if ahead then
                     playKeyAnimation(playerPed)
