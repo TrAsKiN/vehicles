e856f315042890f8023c2f5b8d3a2ef4d961e54f
TrAsKiN <marchal.simon@gmail.com>
2025-02-11 06:16:46 +0100

    style: improve formatting of entityCreated event function

--- server/manager.lua
+++ server/manager.lua
@@ -1,4 +1,4 @@
-RegisterNetEvent('entityCreated', function (entity)
+RegisterNetEvent('entityCreated', function(entity)
     if DoesEntityExist(entity) and GetEntityType(entity) == 2 and not Entity(entity).state.vehiclesInit then
         Entity(entity).state:set('vehiclesInit', true, true)
     end
